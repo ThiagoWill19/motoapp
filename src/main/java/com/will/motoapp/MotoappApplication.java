41e284a198b2b6e642d12adf4d3e1ce67c4ce72a
ThiagoWill19 <thiagopompeu19@gmail.com>
2026-07-24 10:36:41 -0300

    Initialize project structure with Maven, including application properties and necessary configurations

---
+++ src/main/java/com/will/motoapp/MotoappApplication.java
@@ -0,0 +1,13 @@
+package com.will.motoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/will/motoapp/MotoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.motoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
